b6805ef2f4e284dacf4345e4a9a99e7284c6f29e
Ihor Avdiunin <igor.avdiunin@gmail.com>
2024-01-09 11:14:41 +0200

    modified ContactList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,15 @@
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
-import { useSelector } from 'react-redux';
-import { getContactsArr } from '../redux/contacts/contactsSlice';
-import { getFilterValue } from '../redux/filter/filterSlice';
 
 export const App = () => {
-  const contacts = useSelector(getContactsArr);
-  const filter = useSelector(getFilterValue);
-
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter)
-  );
   return (
     <>
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
-      <ContactList contacts={filteredContacts} />
+      <ContactList />
     </>
   );
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,19 @@
 import { ContactItem } from './ContactItem';
 import { StyledContactList } from './ContactList.styled';
+import { getFilterValue } from '../../redux/filter/filterSlice';
+import { getContactsArr } from '../../redux/contacts/contactsSlice';
+import { useSelector } from 'react-redux';
 
-export const ContactList = ({ contacts }) => {
+export const ContactList = () => {
+  const contacts = useSelector(getContactsArr);
+  const filter = useSelector(getFilterValue);
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter)
+  );
   return (
     <StyledContactList>
-      {contacts.map(contact => (
+      {filteredContacts.map(contact => (
         <ContactItem
           key={contact.id}
           id={contact.id}
